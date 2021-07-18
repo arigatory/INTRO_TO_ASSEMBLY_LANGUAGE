b47fbe9f673695568f126e646c8ca1d174ffa257
Ivan <ivan@localhost.localdomain>
2021-07-19 00:57:00 +0300

    update

---
+++ fibonacci/fib.s
@@ -0,0 +1,12 @@
+global  _start
+
+section .text
+_start:
+    xor rax, rax    ; initialize rax to 0
+    xor rbx, rbx    ; initialize rbx to 0
+    inc rbx         ; increment rbx to 1
+loopFib:
+    add rax, rbx    ; get the next number
+    xchg rax, rbx   ; swap values
+    cmp rbx, 10		; do rbx - 10
+    js loopFib		; jump if result is <0
